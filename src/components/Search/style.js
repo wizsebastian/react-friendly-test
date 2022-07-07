c507221b072a337b68a4af1b3d7d5dd8d95d6548
Luis Vasquez <wizsebastian@gmail.com>
2022-07-06 21:14:17 -0400

    add search component with style component file

---
+++ src/components/Search/style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const SearchContainer = styled.div`
+    display: flex;
+    flex-direction: initial;
+    margin-bottom: 8px;
+    input{
+        border: none;
+        padding: 10px;
+        width: 100%;
+    }
+    svg{
+        width: 18px;
+        padding-top: 0.8rem;
+        margin-left: 6px;
+        margin-right: 6px;
+        height: 100%
+    }
+ `
