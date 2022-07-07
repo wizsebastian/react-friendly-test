e96686e436856c450ffb1b80bbc45b9231035035
Luis Vasquez <wizsebastian@gmail.com>
2022-07-06 21:15:48 -0400

    Create a container for the logic

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import CustomContainer from './components/CustomContainer';
 
-import TableManger from './components/Table'
-
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const [searched, setSearched] = useState([]);
-
-  useEffect(() => {
-    fetch("https://api.github.com/users")
-      .then((res) => res.json())
-      .then((data) => {
-        setUsers(data)
-        setSearched(data)
-      })
-  }, [])
-
-
-  const handleSearch = event => {
-    const { value } = event.target;  
-
-   const search =  users.filter((user) => {
-      return user.login.includes(value)
-  })
-    setSearched(search)
-  }
-
   return (
     <div>
-      <input onChange={handleSearch} type="text" id="search" placeholder="Search by USers" />
-      <TableManger data={searched} />
+      <CustomContainer/>
     </div>
   );
 }
