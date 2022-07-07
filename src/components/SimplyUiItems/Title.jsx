5672e2fd3c39cdf5bcb93f7d54bb6ab3c23fb37b
Luis Vasquez <wizsebastian@gmail.com>
2022-07-06 21:15:33 -0400

    Add simply ui items, in this case title component

---
+++ src/components/SimplyUiItems/Title.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Title } from "./style";
+
+const TitleContainer = (props) => {
+  return <Title>{props.text}</Title>;
+};
+
+export default TitleContainer;
---
+++ src/components/SimplyUiItems/style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Title  = styled.h2`
+  font-size: 27px;
+  color: #25252E;
+  padding-top: 16px;
+  padding-bottom: 16px;
+`;
